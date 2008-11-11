bfdc6d165fefd549a452b4bf3ddaddfafe0973e9
Alex <el.zingo@gmail.com>
2008-11-11 15:43:40 +0000

    Added instance method :attribute_is_valid?( attribute ) to test whether or not single fields on your model are valid, and changed the name of the class method: fields_are_valid? to attributes_are_valid?

--- lib/helpful_validations.rb
+++ lib/helpful_validations.rb
@@ -2,17 +2,28 @@
 
 module HelpfulValidations
   def self.included( base )
-    base.extend( ClassMethods::ValidationHelpers )
+    base.send( :include, ClassMethods::ValidationHelpers::InstanceMethods )
+    base.extend( ClassMethods::ValidationHelpers::ClassMethods )
   end
 
   module ClassMethods
     module ValidationHelpers
-      def fields_are_valid?( fields )
-        model = self.new( fields )
-        model.valid? # to run the validations
-        
-        fields.all? do | field, value |
-          model.errors[ field ].nil?
+      module InstanceMethods
+        def attribute_is_valid?( field )
+          valid? # run the validations
+          errors[ field ].nil?
+        end
+      end
+      
+      module ClassMethods
+        def attributes_are_valid?( fields = {} )
+          return true if fields.empty?
+          model = self.new( fields )
+          model.valid? # run the validations
+          
+          fields.all? do | field, value |
+            model.errors[ field ].nil?
+          end
         end
       end
     end
